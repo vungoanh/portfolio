5e97fb54230b0df3449abcedf03909ee8ef70d89
Vu Ngo <ngoanhvu110293@gmail.com>
2024-04-12 15:45:39 +0700

    chore: add UI

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,9 +62,9 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">Hello, I'm Vu Ngo.</span> I'm a{" "}
+        <span className="font-bold">front-end developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
